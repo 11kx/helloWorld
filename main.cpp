a5d595e007e3f9a0f31da359e8f8ce2a5e51b881
孔鑫 <1578818828@qq.com>
2024-09-28 14:46:26 +0800

    对排序接口进行了实现

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 
-int quickSort(int arr[],int size)
+void bubuleSort(int arr[],int size)
 {
-
+    for(int i = 0;i < size;i++)
+    {
+        for(int j = 0;j < size - 1 - i;j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 }
 
-int mian()
+int main()
 {
     int arr[10] = {100,23,32,14,51};
-    quickSort(arr,sizeof(arr)/sizeof(arr[0]));
+    bubuleSort(arr,sizeof(arr)/sizeof(arr[0]));
+    for(auto i : arr)
+    {
+        std::cout << i << " ";
+    }
 }
