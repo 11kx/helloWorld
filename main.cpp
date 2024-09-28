dd99d7b77849a468c2b22edbb872963d27783771
孔鑫 <1578818828@qq.com>
2024-09-28 22:49:37 +0800

    排序改为快排

--- main.cpp
+++ main.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
+#include <algorithm>
 
-void bubuleSort(int arr[],int size)
+int partition(int arr[], int low, int high)
 {
-    for(int i = 0;i < size;i++)
+    int mid_value = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++)
     {
-        for(int j = 0;j < size - 1 - i;j++)
+        if (arr[j] < mid_value)
         {
-            if(arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j+1] = temp;
-            }
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+void quickSort(int arr[], int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid_index = partition(arr, low, high);
+
+    quickSort(arr, low, mid_index - 1);
+    quickSort(arr, mid_index + 1, high);
 }
 
 int main()
 {
-    int arr[10] = {100,23,32,14,51};
-    bubuleSort(arr,sizeof(arr)/sizeof(arr[0]));
-    for(auto i : arr)
+    int arr[10] = {100, 23, 32, 14, 51};
+    quickSort(arr, 0, sizeof(arr) / sizeof(arr[0]) - 1);
+    for (auto i : arr)
     {
         std::cout << i << " ";
     }
